f2447dd432c76dedb0428107914f6876c26fd136
Filip Konstantinos <filipconstantinos@gmail.com>
2022-04-10 17:56:09 +0300

    Switch to xfce

--- .nixos/configuration.nix
+++ .nixos/configuration.nix
@@ -39,8 +39,8 @@
 
   # Enable the Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
-  # services.xserver.desktopManager.pantheon.enable = true;
-  services.xserver.windowManager.xmonad.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.windowManager.xmonad.enable = false;
 
   # Configure keymap in X11
   services.xserver.layout = "us,el";
