08b876a5f8f121542d0bb3ff52482b96453655a4
Filip Konstantinos <filipconstantinos@gmail.com>
2023-01-31 21:56:43 +0200

    Set touchpad and add htop

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -94,7 +94,12 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput = {
+    enable = true;
+    touchpad.tapping = true;
+    touchpad.naturalScrolling = false;
+    touchpad.disableWhileTyping = true;
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.filipkon = {
@@ -127,6 +132,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     emacs
     wget git ripgrep
+    htop
   ];
 
   # Specify nerd-fonts to install
