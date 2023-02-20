b405cb54933dd5ecc76b4c4aa6c3f6db62f31f21
Filip Konstantinos <filipconstantinos@gmail.com>
2023-02-20 23:23:30 +0200

    Init flake

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -174,4 +174,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
+  # use flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
---
+++ nixos/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      user = "filipkon";
+      location = "$HOME/dotfiles/nixos";
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      lib = nixpkgs.lib;
+    in
+      {
+        nixosConfigurations = {
+          ideapad = lib.nixosSystem {
+            inherit system;
+            modules = [ ./configuration.nix ];
+          };
+        };
+      };
+}
